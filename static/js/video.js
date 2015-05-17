0da03ec8275711083fc7769bfda6d7fe80da7937
Tadej Novak <tadej@tano.si>
2015-05-17 10:29:32 +0200

    UX improvements - part 2

--- static/js/video.js
+++ static/js/video.js
@@ -4,24 +4,31 @@
   var firstScriptTag = document.getElementsByTagName('script')[0];
   firstScriptTag.parentNode.insertBefore(videoTag, firstScriptTag);
 
-  var player;
+  var player = null;
+  var playerReady = false;
   /*function onYouTubeIframeAPIReady() {
     
   }*/
 
-  function initPlayer() {
+  function initPlayer(id) {
     player = new YT.Player('player', {
       height: '100%',
       width: '100%',
-      // videoId: '',
-      // events: {
-      //   'onReady': onPlayerReady,
-      //   'onStateChange': onPlayerStateChange
-      // }
+      videoId: id,
+      events: {
+        'onReady': onPlayerReady
+      }
     });
   }
 
+  function onPlayerReady() {
+    player.playVideo();
+    playerReady = true;
+  }
+
   function togglePause() {
+    if (!player || !playerReady) return;
+
     if (player.getPlayerState() == 2 || player.getPlayerState() == 0 || player.getPlayerState() == 5) {
       player.playVideo();
     } else {
